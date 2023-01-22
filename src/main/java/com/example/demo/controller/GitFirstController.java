7f75c9b9426da93ebd736125bc0a0302baea7199
redrouthuvaschalya <121494533+redrouthuvaschalya@users.noreply.github.com>
2023-01-22 20:08:24 +0530

    api new added by vasu

--- src/main/java/com/example/demo/controller/GitFirstController.java
+++ src/main/java/com/example/demo/controller/GitFirstController.java
@@ -31,5 +31,11 @@ public class GitFirstController {
 	public String HelloApi4() {
 		return "Hello World4";
 	}
+   @GetMapping("/api/hello5")
+	
+	
+	public String HelloApi5() {
+		return "Hello World5";
+	}
 
 }
